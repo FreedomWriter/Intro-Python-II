bbff1e64d822cbcc135fb4624a35bbf19ff47a88
FreedomWriter <natalienetworks@gmail.com>
2020-07-14 20:42:16 -0500

    added methods to player

--- src/player.py
+++ src/player.py
@@ -7,8 +7,15 @@
 
 
 class Player():
-    def __init__(self, name, current_room):
+    def __init__(self, name="", current_room="", items=[]):
         self.name = name
         self.current_room = current_room
+        self.items = items
+
+    def get_item(self, item):
+        self.items.append(item)
+
+    def drop_item(self, item):
+        self.items.remove(item)
     # def __str__(self):
     #     return '{self.name} is currently in {self.current_room}'.format(self=self)
